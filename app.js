53cad622263094fea7b181f3a0a6159c0d94096c
José Francisco Tello Ropero <jfrantello@gmail.com>
2014-10-13 16:16:08 +0200

    RGB Led web controller

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 "use strict";
 var five = require("johnny-five");
+var http = require("http");
+var url = require("url");
+
 var board = new five.Board();
 
 board.on("ready", function() {
-  var pin = 13;
-  var led = new five.Led(pin);
-  led.strobe(500);
-  // Inject led object into REPL session
-  this.repl.inject({
+  var led = new five.Led.RGB({
+    pins: {
+      red: 9,
+      green: 10,
+      blue: 11
+    }
+  });
+
+  board.repl.inject({
     led: led
   });
+
+  led.on();
+
+  var server = http.createServer(function(req, res) {
+    var urlParts = url.parse(req.url, true);
+    var query = urlParts.query;
+    led.color(query.color);
+    res.end(query.color);
+  });
+
+  var port = 8080;
+  server.listen(port);
 });
